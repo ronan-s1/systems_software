de696858f0e4930a8c8710f6e0dfe90f8d003451
ronan-s1 <singpurwala13@gmail.com>
2024-02-16 11:29:28 +0000

    .

---
+++ lab4/countdown.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() 
+{
+    time_t now;
+    struct tm newyear;
+    double seconds;
+
+    time(&now);
+    newyear = *localtime(&now);
+
+    newyear.tm_hour = 20;
+    newyear.tm_min = 45;
+    newyear.tm_sec = 0;
+
+    seconds = difftime(now, mktime(&newyear));
+
+    if (seconds < 0) 
+    {
+        seconds = -seconds; 
+
+        while (seconds > 0) 
+        {
+            printf("%f seconds left\n", seconds);
+            sleep(1);
+            time(&now);
+            seconds = difftime(now, mktime(&newyear));
+        }
+    }
+
+    printf("time up\n");
+
+    return 0;
+}
